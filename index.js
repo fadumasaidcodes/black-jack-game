aa19f962cf9c2c2631390fa954b03a873aa2a97e
fadumasaidcodes <fadumasaid2022@gmail.com>
2023-04-16 23:14:50 +0100

    added the test part

---
+++ index.js
@@ -0,0 +1,11 @@
+ // 1. Create two variables, firstCard and secondCard. 
+
+// Set their values to a random number between 2-11
+var firstCard = 4;
+var secondCard = 10;
+
+// 2. Create a variable, sum, and set it to the sum of the two cards.
+
+var sum = firstCard + secondCard;
+
+// console.log(sum)
