b46e2e73ae144be9d85d482db6b3a94026fa72de
fadumasaidcodes <fadumasaid2022@gmail.com>
2023-04-17 00:01:08 +0100

    added another if else

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let firstCard = 6
+/*
+let firstCard = 9
 let secondCard = 9
 let sum = firstCard + secondCard
 
@@ -8,4 +9,21 @@ if (sum < 21) {
     console.log("Wohoo! You've got Blackjack! 🥳")
 } else {
     console.log("You're out of the game! 😭")
+}
+*/
+
+// Check if the person is old enough to enter the nightclub (21)
+// Log a suitable message to the console in both cases
+
+let age = 19
+
+// if less than 21 -> "You can not enter the club!"
+// else            -> "Welcome
+
+if(age === 21) {
+    console.log(" you are old enough to enter the nightClub")
+} else if ( age < 21){
+    console.log("You can not enter the club!")
+} else {
+    console.log("Welcome")
 }
