8ced5502e1b80e509c77aec468824a02659ae121
fadumasaidcodes <fadumasaid2022@gmail.com>
2023-04-19 05:02:56 +0100

    reassign the message variable

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ if (sum <= 20) {
 console.log(isAlive)
 */
 
+/*
 console.log(4 === 3)  //  false 
 console.log(5 > 2)    //  true
 console.log(12 > 12)  // false 
@@ -72,3 +73,32 @@ console.log(3 < 0)    // false
 console.log(3 >= 3)   //  true 
 console.log(11 <= 11) //true
 console.log(3 <= 2)   // false
+
+*/
+
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard 
+let hasBlackJack = false
+let isAlive = true
+// 1. Declare a variable called message and assign its value to an empty string
+
+let message = ""
+
+// 2. Reassign the message variable to the string we're logging out
+
+
+
+if (sum <= 20) {
+ message = "Do you want to draw a new card? 🙂"
+} else if (sum === 21) {
+  message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true
+} else {
+   message = "You're out of the game! 😭"
+    isAlive = false
+}
+
+// 3. Log it out!
+
+console.log(message)
